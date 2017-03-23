d0c099b38920c78002df5b3407a431b6c28996da
Venkata Reddy Bhavanam <venkatareddy.cs@gmail.com>
2017-03-23 15:18:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Contenders from './components/contenders';
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Contenders />
+    );
+  }
+}
+
+export default App;
