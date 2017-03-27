81d47a810cc6be62acb2a11f2139cbfd139a055d
Venkata Reddy Bhavanam <venkatareddy.cs@gmail.com>
2017-03-27 14:42:31 +0530

    Pull api code into the repo

---
+++ api/db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Entry.create!(name: "DHH", winner: false)
+Entry.create!(name: "Tenderlove", winner: false)
+Entry.create!(name: "Avdi", winner: false)
+Entry.create!(name: "Jose", winner: false)
+Entry.create!(name: "Ryan", winner: false)
+Entry.create!(name: "Dan", winner: false)
+Entry.create!(name: "Jim", winner: false)
